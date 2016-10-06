d7fe2ea9cc90f73776ac1c50c63c4189b72268ee
Jangho <jhc@dgu.edu>
2016-10-06 21:32:43 +0900

    add increment, decrement button

--- app/src/main/java/io/github/jangho/udacity/MainActivity.java
+++ app/src/main/java/io/github/jangho/udacity/MainActivity.java
@@ -19,12 +19,20 @@ public class MainActivity extends ActionBarActivity {
     }
 
     /**
-     * This method is called when the order button is clicked.
+     * This method is called when the plus button is clicked.
      */
-    public void submitOrder(View view) {
-        int numberOfCoffees =2;
-        display(numberOfCoffees);
-        displayPrice(numberOfCoffees*5);
+    public void increment(View view) {
+        int quantity = 3;
+        display(quantity);
+    }
+
+    /**
+     * This method is called when the minus button is clicked.
+     */
+
+    public void decrement(View view) {
+        int quantity = 1;
+        display(quantity);
     }
 
     /**
